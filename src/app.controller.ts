1610c03d7337229c28a99132ac36dc4e72ddb45e
Brandon alves <brandon.alves@redeinova.com>
2024-02-21 18:07:48 -0300

    ajuste documentação swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
-import {Body, Controller, Get, HttpException, HttpStatus, Param, Post, Query, Res} from '@nestjs/common';
+import {Body, Controller, Get, HttpException, HttpStatus, Param, Post, Query, Res, UseGuards} from '@nestjs/common';
 import { AppService } from './app.service';
 import axios, {AxiosResponse} from "axios";
 import { Response } from 'express'
-import {ApiQuery, ApiTags} from "@nestjs/swagger";
+import {ApiBearerAuth, ApiQuery, ApiTags} from "@nestjs/swagger";
 import {DatabaseService} from "./database/database.service";
 import {Bit, Char, DateTime, Int, Request, Table, VarChar} from "mssql";
 import {QueryParamsCatalItemTypes} from "./app/catalogo/catalogo-item.controller";
 import {ExecutarDto} from "./dto-others/executar.dto";
+import {AuthGuard} from "./app/auth/auth.guard";
 
 export type QueryParamsSegmentoTypes = {
   empresa: string
@@ -21,6 +22,7 @@ export type QueryParamsCepTypes = {
   sequencial: number
 };
 @ApiTags('Outros')
+@ApiBearerAuth()
 @Controller()
 export class AppController {
   constructor(
@@ -100,7 +102,8 @@ export class AppController {
       });
     }
   }
-  @Post(['executar', 'Executar'])
+  @UseGuards(AuthGuard)
+  @Post(['executar'])
   async executeProcess(@Res() response: Response,
                  @Body() bodyExecute: ExecutarDto) {
     try {
@@ -139,6 +142,77 @@ export class AppController {
       return response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
         message: 'Erro interno de servidor',
         erro,
+      });
+    }
+  }
+
+  @UseGuards(AuthGuard)
+  @ApiQuery({required: false, name: 'limite'})
+  @ApiQuery({required: false, name: 'sequencial'})
+  @Get('formapgto')
+  async findFormasPgto(@Res() response: Response,
+                      @Query() queryParams: {limite: number
+                        idioma: string
+                        sequencial: number}) {
+    try {
+      const request = new Request(this.database.connection());
+      /**ADD VARIABLES IN PROCEDURE*/
+      request.input('limite', queryParams.sequencial ? 1 : queryParams.limite || 500);
+      request.input('sequencial', queryParams.sequencial || 0);
+      // await request.input('idioma', queryParams.idioma);
+      /**EXECUTE PROCEDURE*/
+      const result = await request.execute('sp_Api_FormaPgto_Obter');
+      /**GET RETURN PROCEDURE*/
+      const returnProcedure = result.recordset;
+      /**VALIDATIONS AND RETURNS FOR CLIENT*/
+      returnProcedure && returnProcedure.forEach((resp) => {
+        if (resp.erro === "true" || resp.erro === true) {
+          return response.status(400).json(resp);
+        }
+      })
+      return response.status(200).json(returnProcedure);
+    } catch (erro) {
+      console.log(erro);
+      throw new HttpException(erro, HttpStatus.INTERNAL_SERVER_ERROR, {
+        cause: erro,
+      });
+    }
+  }
+
+  @UseGuards(AuthGuard)
+  @ApiQuery({required: true, name: 'empresa'})
+  @ApiQuery({required: false, name: 'limite'})
+  @ApiQuery({required: true, name: 'sequencial'})
+  @Get('pedidoformapgto')
+  async findPedidoFormaPgto(@Res() response: Response,
+                      @Query() queryParams: QueryParamsSegmentoTypes) {
+    if (!queryParams.empresa) {
+      return response.status(HttpStatus.BAD_REQUEST).json({
+        message: 'O parametro [empresa] deve ser informado.',
+      });
+    }
+    try {
+      const request = new Request(this.database.connection());
+      /**ADD VARIABLES IN PROCEDURE*/
+      request.input('empresa', queryParams.empresa);
+      request.input('limite', queryParams.sequencial ? 1 : queryParams.limite || 500);
+      request.input('sequencial', queryParams.sequencial || 0);
+      // await request.input('idioma', queryParams.idioma);
+      /**EXECUTE PROCEDURE*/
+      const result = await request.execute('sp_Api_PedidoFormaPgto_Obter');
+      /**GET RETURN PROCEDURE*/
+      const returnProcedure = result.recordset;
+      /**VALIDATIONS AND RETURNS FOR CLIENT*/
+      returnProcedure && returnProcedure.forEach((resp) => {
+        if (resp.erro === "true" || resp.erro === true) {
+          return response.status(400).json(resp);
+        }
+      })
+      return response.status(200).json(returnProcedure);
+    } catch (erro) {
+      console.log(erro);
+      throw new HttpException(erro, HttpStatus.INTERNAL_SERVER_ERROR, {
+        cause: erro,
       });
     }
   }
